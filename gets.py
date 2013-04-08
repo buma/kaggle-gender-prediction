6220247c9f66c614188b9b569f73d5b46429fca7
Marko Burjek <s0mebody.slo@gmail.com>
2013-04-08 21:45:14 +0200

    Now gets outputs all features

--- gets.py
+++ gets.py
@@ -1,6 +1,11 @@
 """Gets feature name from prefixes from:https://www.kaggle.com/c/icdar2013-gender-prediction-from-handwriting/forums/t/3982/looking-for-a-feature"""
 template = "{0} = get_columns_with_prefix('{1}')"
 f = open("prefixs")
+
+features = []
 for line in f.read().splitlines():
     feature_name, prefix = line.split(":")
     print template.format(feature_name, prefix)
+    features.append(prefix)
+
+print features
