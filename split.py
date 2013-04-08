44b4b7122d727cb29a3bc2329f2a79b88d53d8f9
Marko Burjek <s0mebody.slo@gmail.com>
2013-04-08 14:41:48 +0200

    Split in train and test set

---
+++ split.py
@@ -0,0 +1,52 @@
+'''
+split a file into train test and validation set on writer
+Usage: split.py'
+'''
+
+import random
+from data_io import DataIO
+import os
+import pandas as pd
+from os.path import join as path_join
+
+dio = DataIO("Settings_submission.json")
+dio1 = DataIO("Settings.json")
+
+input_file = dio.train_file
+output_file1 = dio1.train_file
+output_file2 = dio1.valid_file
+
+print "Input: %s " % input_file
+print "Train file: %s " % output_file1
+print "Validation file: %s " % output_file2
+
+store = pd.HDFStore(
+    path_join(dio.data_dir, "data", "processed", "train_w_answers.h5")
+)
+
+train = store["train_w_answers"]
+print train
+random.seed(42)
+all_writers = train.writer.unique()
+print "all writers:", len(all_writers)
+random.shuffle(all_writers)
+#first 80%
+first = int(round(len(all_writers) * 0.8))
+train_writers = all_writers[0:first]
+valid_writers = all_writers[first:]
+
+print "Train subjects:", len(train_writers)
+print "Valid subjects:", len(valid_writers)
+
+run = raw_input("OK (Y/N)?")
+print run
+if run != "Y":
+    os.exit()
+
+store["train_train"] = train[train['writer'].isin(train_writers)].copy()
+
+store["train_test"] = train[train['writer'].isin(valid_writers)].copy()
+
+print store
+
+store.close()
