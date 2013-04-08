31893d57236a2475d2fbf17c0429db616ae1ef93
Marko Burjek <s0mebody.slo@gmail.com>
2013-04-08 14:06:25 +0200

    Joins answers to training data

---
+++ join_answers.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""It joins training file with answers"""
+from data_io import DataIO
+import pandas as pd
+from os.path import join as path_join
+
+dio = DataIO("Settings_submission.json")
+
+training_data = pd.read_csv(dio.train_file)
+
+data_path = dio.train_file.split("/")[:-1]
+
+filename = path_join(dio.data_dir, "data", "raw", "train_answers.csv")
+
+training_answers = pd.read_csv(filename)
+
+train_with_answers = pd.merge(training_data, training_answers, on='writer')
+
+train_with_answers.to_csv(
+    path_join(dio.data_dir, "data", "processed", "train_w_answers.csv")
+)
+
+store = pd.HDFStore(
+    path_join(dio.data_dir, "data", "processed", "train_w_answers.h5")
+)
+
+store["train_w_answers"] = train_with_answers
+
+store.close()
